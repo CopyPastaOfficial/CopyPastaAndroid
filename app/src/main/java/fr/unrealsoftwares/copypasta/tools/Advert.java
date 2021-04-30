2b4d40feab49d179b531fb926fa432ca9a9a605c
Remi ALBAN <remi@github.com>
2021-04-30 07:37:07 +0200

    Remove logs

--- app/src/main/java/fr/unrealsoftwares/copypasta/tools/Advert.java
+++ app/src/main/java/fr/unrealsoftwares/copypasta/tools/Advert.java
@@ -1,7 +1,6 @@
 package fr.unrealsoftwares.copypasta.tools;
 
 import android.app.Activity;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 
@@ -60,12 +59,8 @@ public class Advert {
         Random random=new Random();
         int number = random.nextInt(100);
         int proportion = Meta.getAdvertProportion(activity);
-        Log.i("DEBUG", "IN ADVERT");
-        Log.i("DEBUG", Meta.getVersion(activity));
-        Log.i("DEBUG", String.valueOf(number));
         if(Meta.getVersion(activity).trim().equals("lite") && number <= proportion)
         {
-            Log.i("DEBUG", "IN LITE");
             MobileAds.initialize(activity, new OnInitializationCompleteListener() {
                 @Override
                 public void onInitializationComplete(InitializationStatus initializationStatus) {}
@@ -75,8 +70,6 @@ public class Advert {
             InterstitialAd.load(activity,key, adRequest, new InterstitialAdLoadCallback() {
                 @Override
                 public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
-                    Log.i("DEBUG", "IN ONADLOAD");
-
                     isLoaded = true;
                     ad = interstitialAd;
                     if(isWantLoad)
@@ -87,8 +80,6 @@ public class Advert {
 
                 @Override
                 public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
-                    Log.i("DEBUG", "IN LOAD AD ERROR");
-
                     isLoaded = true;
                     ad = null;
                     if(isWantLoad)
@@ -118,21 +109,17 @@ public class Advert {
      */
     public void show(Callback callback)
     {
-        Log.i("DEBUG", "SHOW");
         this.callback = callback;
         if(Meta.getVersion(activity).trim().equals("pro"))
         {
-            Log.i("DEBUG", "IN PRO");
             callback.onAdvertLoaded();
             return;
         }
         if(isLoaded)
         {
-            Log.i("DEBUG", "IN LOAD");
             showAdvert();
         } else
         {
-            Log.i("DEBUG", "IN ELSE");
             isWantLoad = true;
         }
     }
@@ -142,7 +129,6 @@ public class Advert {
      */
     public void showAdvert()
     {
-        Log.i("DEBUG", "IN SHOW ADVERT");
         ad.setFullScreenContentCallback(new FullScreenContentCallback(){
             @Override
             public void onAdDismissedFullScreenContent() {
